fa53602a64e8a11e76f28b89fce37874a35cca04
MrVanDaime - Home <joaocamini@live.com>
2024-06-19 08:22:53 -0300

    Refactor: extracted and renamed difficultyColors, extracted itemBg and panelBg from difficultyColors[question.difficulty] to remove duplication, created new accordion button handler to fix not working toggle option

--- src/components/QuizList.jsx
+++ src/components/QuizList.jsx
@@ -9,48 +9,51 @@ import {
   Box,
 } from "@chakra-ui/react";
 
+const difficultyColors = {
+  easy: {
+    itemBg: "#198754",
+    panelBg: "#116840",
+  },
+  moderate: {
+    itemBg: "#ffa001",
+    panelBg: "#cd8000",
+  },
+  hard: {
+    itemBg: "#c72827",
+    panelBg: "#a92222",
+  },
+};
+
 export default function QuizList({ questions }) {
   const [openIndex, setOpenIndex] = useState(-1);
-
-  const difficulty = {
-    easy: {
-      itemBg: "#198754",
-      panelBg: "#116840",
-    },
-    moderate: {
-      itemBg: "#ffa001",
-      panelBg: "#cd8000",
-    },
-    hard: {
-      itemBg: "#c72827",
-      panelBg: "#a92222",
-    },
-  };
 
   useEffect(() => {
     setOpenIndex(-1);
   }, [questions]);
 
+  const handleAccordionButtonClick = (index) => {
+    setOpenIndex(index === openIndex ? -1 : index);
+  };
+
   return (
     <Accordion index={openIndex} allowToggle>
-      {questions.map((question, index) => (
-        <AccordionItem
-          key={question.id}
-          backgroundColor={difficulty[question.difficulty].itemBg}
-        >
-          <AccordionButton onClick={() => setOpenIndex(index)}>
-            <Box as="span" flex="1" textAlign="left">
-              <b>{question.question}</b>
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-          <AccordionPanel
-            backgroundColor={difficulty[question.difficulty].panelBg}
-          >
-            {question.answer}
-          </AccordionPanel>
-        </AccordionItem>
-      ))}
+      {questions.map((question, index) => {
+        const { itemBg, panelBg } = difficultyColors[question.difficulty];
+
+        return (
+          <AccordionItem key={question.id} backgroundColor={itemBg}>
+            <AccordionButton onClick={() => handleAccordionButtonClick(index)}>
+              <Box as="span" flex="1" textAlign="left">
+                <b>{question.question}</b>
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+            <AccordionPanel backgroundColor={panelBg}>
+              {question.answer}
+            </AccordionPanel>
+          </AccordionItem>
+        );
+      })}
     </Accordion>
   );
 }
