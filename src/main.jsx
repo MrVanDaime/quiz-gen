229bf25c5d8ec58e5a691c705b72512ab27fa452
MrVanDaime - Home <joaocamini@live.com>
2024-07-13 11:43:26 -0300

    Update: set dark theme by default

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider, ColorModeScript, Container } from "@chakra-ui/react";
+
+import theme from "./theme.js";
 import App from "./App.jsx";
-import { ChakraProvider, Container } from "@chakra-ui/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <Container maxW="1280px" p="3rem" m="0 auto" textAlign="center">
         <App />
       </Container>
